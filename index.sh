74e41abf88f223a527a3987cbe3ed950c59389b7
afeiship <1290657123@qq.com>
2019-03-19 08:56:27 +0800

    add: list & clear

--- index.sh
+++ index.sh
@@ -4,6 +4,8 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias when-init='wheneverize .'
 alias when-list='whenever'
+alias when-list-all='crontab -l'
+alias when-clear='whenever -c'
 alias when-update='whenever --update-crontab'
 
 unset ROOT_PATH;
